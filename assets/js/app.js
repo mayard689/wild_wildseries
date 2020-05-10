34c63548f84ee0c39c60c37a06395cbe64e9286d
mayard689 <doc-albert@laposte.net>
2020-05-10 18:19:58 +0200

    add jQuery and popper.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,10 +8,23 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.scss';
 
+//load the jQuery and import the function from jQuery
 const $ = require('jquery');
+
+//load the JS bootstrap part - note that bootstrap doesn't export anything
 require('bootstrap');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+//import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+
+if(jQuery){
+    alert("jQuery est chargé");
+}
+else{
+    alert("jQuery n'est pas chargé");
+}
+
+
