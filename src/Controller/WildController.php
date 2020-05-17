a6d3b9b64a9b27ed674779f6ee5fae76ebee9cad
mayard689 <doc-albert@laposte.net>
2020-05-17 23:33:48 +0200

    add route class prefix

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -8,6 +8,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class WildController
+ * @package App\Controller
+ *
+ * @route("wild", name="_wild")
+ */
 class WildController extends AbstractController
 {
 
@@ -15,7 +21,7 @@ class WildController extends AbstractController
      * @param $title
      * @return Response
      *
-     * @route("wild/{slug<[a-z0-9-]+>}", name="wild_show")
+     * @route("/{slug<[a-z0-9-]+>}", name="wild_show")
      */
     public function show(string $slug="Aucune série sélectionnée, veuillez choisir une série")
     {
